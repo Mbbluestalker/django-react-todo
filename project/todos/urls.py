0ca84e541fb373366b92806ed43799e412366cc7
Akorede Olawole <akoredeolawole@gmail.com>
2022-12-02 00:19:52 +0100

    created a urls.py file

---
+++ project/todos/urls.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+from .views import TodoViewset
+
+
+router = routers.DefaultRouter()
+router.register(r'api/todos', TodoViewset, 'todos')
+
+urlpatterns = router.urls
